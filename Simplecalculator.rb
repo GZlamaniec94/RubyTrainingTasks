e54a5ee5cf00cae0c67bb220086b40e26905ea71
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-02-24 14:51:43 +0100

    Calculator

---
+++ Simplecalculator.rb
@@ -0,0 +1,36 @@
+puts "Simple calculator"
+30.times{print "-" }
+puts
+puts "First number"
+num_1 = gets.chomp
+
+puts "What u want to do"
+operator = gets.chomp
+puts "Second number"
+num_2 = gets.chomp
+
+def calculating(num_1, num_2, operator)
+    if  operator == "*"
+    return num_1.to_i * num_2.to_i
+    
+elsif operator == "+"
+    return num_1.to_i + num_2.to_i
+
+elsif operator == "-"
+    return num_1.to_i - num_2.to_i
+
+elsif operator == "/"
+    if num_2.to_i == 0
+        puts "Cannot divide by 0"
+    else 
+    return num_1.to_i / num_2.to_i
+    end
+    
+else operator != "*" || "+" || "-" || "/"
+    puts "Wrong operator"
+end
+end
+
+ final_number = calculating(num_1, num_2, operator)
+
+puts final_number
---
+++ main.rb
@@ -0,0 +1,7 @@
+
+sample_hash = { :a=> 1, :b=> 2, :c=>3}
+
+ 
+ sample_hash.each do |key, value|
+    puts "The class for key #{key.class} and the value is #{value.class}"
+ end
